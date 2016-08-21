806118d609a44759cf6b284e7faf9d696dd9a89d
Andrew Welleck <andrew8838@gmail.com>
2016-08-21 01:02:05 -0500

    Fixed links, added hover color.

--- projects.js
+++ projects.js
@@ -1,6 +1,6 @@
 function myMoveFunction() {
     // menu pops up
-	var projects_div = document.getElementById("project");
+    var projects_div = document.getElementById("project");
     projects_div.style.display = "inline";
 }
 
