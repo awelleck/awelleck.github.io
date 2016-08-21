3c8862bedde3127bf596ab56c9380b07d45689e6
Andrew Welleck <andrew8838@gmail.com>
2016-08-20 23:07:56 -0500

    Fixed GitHub, LinkedIn, and Mail icons, added Projects dropdown.

---
+++ projects.js
@@ -0,0 +1,10 @@
+function myMoveFunction() {
+    // menu pops up
+	var projects_div = document.getElementById("project");
+    projects_div.style.display = "inline";
+}
+
+function myOutFunction() {
+	var projects_div = document.getElementById("project");
+	projects_div.style.display = "none";
+}
