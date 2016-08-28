55745b2523a2e0082053a6053d09374bee220333
Andrew Welleck <andrew8838@gmail.com>
2016-08-28 18:55:51 -0500

    Fixed dice.js and added custom .svg images made with Inkscape.

--- dice.js
+++ dice.js
@@ -1,6 +1,8 @@
+var pics = ["img/die1.svg", "img/die2.svg", "img/die3.svg", "img/die4.svg", "img/die5.svg", "img/die6.svg"];
+
 function diceRoller() {
     var x, y;
-    x = Math.floor((Math.random() * 6) + 1);
-    y = Math.floor((Math.random() * 6) + 1);
-    document.getElementById("rollButton").innerHTML = x +" "+ y;
+    x = Math.floor((Math.random() * 6));
+    y = Math.floor((Math.random() * 6));
+     document.getElementById("rollButton").innerHTML = "<img src=\"" + pics[x] + "\"/> <img src=\"" + pics[y] + "\"/>"; 
 }
