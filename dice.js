8e9ad04742921c1a3fc210dc9bb7f2878db3098e
Andrew Welleck <andrew8838@gmail.com>
2016-08-29 21:45:59 -0500

    Fixed spacing, added comments.

--- dice.js
+++ dice.js
@@ -1,8 +1,8 @@
 var pics = ["img/die1.svg", "img/die2.svg", "img/die3.svg", "img/die4.svg", "img/die5.svg", "img/die6.svg"];
 
 function diceRoller() {
-    var x, y;
-    x = Math.floor((Math.random() * 6));
-    y = Math.floor((Math.random() * 6));
-     document.getElementById("rollButton").innerHTML = "<img src=\"" + pics[x] + "\"/> <img src=\"" + pics[y] + "\"/>"; 
+  var x, y;
+  x = Math.floor((Math.random() * 6));
+  y = Math.floor((Math.random() * 6));
+  document.getElementById("rollButton").innerHTML = "<img src=\"" + pics[x] + "\"/> <img src=\"" + pics[y] + "\"/>";
 }
