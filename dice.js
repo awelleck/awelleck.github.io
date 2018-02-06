d0f8c479c75e0739caffbc84883658706151dcb3
Andrew Welleck <andrew8838@gmail.com>
2018-02-05 20:44:08 -0600

    Full resume available on request

--- dice.js
+++ dice.js
@@ -1,3 +1,7 @@
+/**
+ * JS for dice and resume alert
+ * Andrew Welleck
+ */
 var pics = ["img/die1.svg", "img/die2.svg", "img/die3.svg", "img/die4.svg", "img/die5.svg", "img/die6.svg"];
 
 function diceRoller() {
@@ -6,3 +10,9 @@ function diceRoller() {
   y = Math.floor((Math.random() * 6));
   document.getElementById("rollButton").innerHTML = "<img src=\"" + pics[x] + "\"/> <img src=\"" + pics[y] + "\"/>";
 }
+
+$(document).ready(function () {
+  $('.resume_button').click(function () {
+    $('.resume').append("<div class='alert alert-info alert-dismissable' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button><strong>Info:</strong> Full r&eacute;sum&eacute; available on request.</div>")
+  })
+});
